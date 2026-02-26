1d12fb75fd4f5ba38cd09a88f39e33dac5f9dc75
cpovirk <cpovirk@google.com>
2026-02-26 14:08:16 -0800

    Test `ClosingFuture` leak behavior.
    RELNOTES=n/a
    PiperOrigin-RevId: 875882754

---
+++ guava-tests/test/com/google/common/util/concurrent/ClosingFutureLeakTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2026 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.util.concurrent;
+
+import static com.google.common.testing.GcFinalization.awaitClear;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+
+import com.google.common.util.concurrent.AbstractClosingFutureTest.TestCloseable;
+import java.lang.ref.WeakReference;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+/** Tests for {@link ClosingFuture} leaks. */
+@NullUnmarked
+public class ClosingFutureLeakTest extends TestCase {
+  public void testLeak() {
+    TestCloseable closeable = new TestCloseable("closeable");
+    awaitClear(leakClosingFuture(closeable));
+    assertThat(closeable.awaitClosed()).isTrue();
+  }
+
+  private WeakReference<ClosingFuture<String>> leakClosingFuture(TestCloseable closeable) {
+    ClosingFuture<String> willCloseCloseable =
+        ClosingFuture.submit(
+            closer -> {
+              closer.eventuallyClose(closeable, directExecutor());
+              return "";
+            },
+            directExecutor());
+    return new WeakReference<>(willCloseCloseable);
+  }
+}
